68c80df3754da195453d41d3022c985220b77bab
aobfilho <antonioobfilho@gmail.com>
2022-03-31 10:02:59 -0300

    Removing id field and celling class

--- src/main/java/com/devsuperior/bds04/dto/CityInsertDTO.java
+++ src/main/java/com/devsuperior/bds04/dto/CityInsertDTO.java
@@ -3,28 +3,22 @@ package com.devsuperior.bds04.dto;
 import com.devsuperior.bds04.entities.City;
 
 import javax.validation.constraints.NotBlank;
-import java.io.Serializable;
 
-public class CityInsertDTO implements Serializable {
-	private static final long serialVersionUID = 1L;
+public class CityInsertDTO {
 
-	private Long id;
 	@NotBlank(message = "Campo requerido")
 	private String name;
 
-	public CityInsertDTO(Long id, String name) {
-		this.id = id;
+	public CityInsertDTO(){}
+
+	public CityInsertDTO(String name) {
 		this.name = name;
 	}
 
 	public CityInsertDTO(City entity) {
-		id = entity.getId();
 		name = entity.getName();
 	}
 
-	public Long getId() {
-		return id;
-	}
 	public String getName() {
 		return name;
 	}
